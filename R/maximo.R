75a4368e1c41b3e9bd927463b6f7e6ce93b02f18
amarent <amarent@hotmail.com>
2014-12-19 11:47:09 -0600

    commit decimonoveno j

--- R/maximo.R
+++ R/maximo.R
@@ -2,5 +2,5 @@ maximo=function(rubro,mes,n) {
 x<-getdata98(rubro,mes,n)
 x<-x[!is.na(x[,1]),]
 max<-max(x)
-return(list(message=paste("el maximo es", max,"  "))
+return(list(message=paste("el maximo es", max,"  ")))
 }
