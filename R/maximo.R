ee9ccc71983e571cf7b698c4c8b8d842f1d531b5
amarent <amarent@hotmail.com>
2014-12-23 10:25:32 -0600

    1223 01

--- R/maximo.R
+++ R/maximo.R
@@ -1,6 +1,8 @@
 maximo=function(rubro,mes,n) {
-x<-getdata98(rubro,mes,n)
-x<-x[!is.na(x[,1]),]
+#x<-getdata98(rubro,mes,n)
+#x<-x[!is.na(x[,1]),]
+
+x<-c(2,4,7,9,4,2,6,8,4,2,42,64,8,23,12)
 
 max<-paste("Maximo:",max(x))
 
@@ -15,11 +17,11 @@ promedio<-paste("Promedio:",mean(x))
 
 l<-list(max,min,asimetria,sd,promedio)
 
-#plot(-1:1, -1:1, type = "b", xlab = "", ylab = "")
 
-
-
-plot(-1:1,-1:1,type=b,xlab="",ylab="")
-text(x=-.8,y=c(-.8,-.4,0,.4,.8),labels=l)
+plot(-1:1,-1:1,type="n",xlab="",ylab="",bty="n")
+par(new=T)
+text(x=-.5,y=c(-.8,-.4,0,.4,.8),labels=l,adj=0)
+par(new=T)
+plot(-1:1,-1:1,type="n",xlab="",ylab="",bty="n")
 #list(message=paste("Maximo: \n", max,"Minimo: \n", min,"Asimetria:\n",asimetria,"Desviacion estandar: \n", sd,"Promedio: \n", promedio))
 }
