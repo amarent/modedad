940a5ba307a87fec0665d3a4b164a1e9052d5443
amarent <amarent@hotmail.com>
2014-12-23 16:46:08 -0600

    1223 22

--- R/histograma.R
+++ R/histograma.R
@@ -5,9 +5,9 @@ histograma <- function(rubro, mes, n)
 
 
 
-mydata<-c(2,3,4,56,3,2,24,8,4,3,23,456,78,64,3,56,89,89)
-h<-hist(mydata,main=paste("Histograma de ",colnames(mydata)),plot=F)
+#mydata<-c(2,3,4,56,3,2,24,8,4,3,23,456,78,64,3,56,89,89)
+h<-hist(mydata,plot=F)
 h$density<-with(h,100*density*diff(breaks)[1])
 labs<-paste(round(h$density),"%",sep="")
-plot(h,freq=F,labels=labs,ylim=c(0,1.08*max(h$density),xlab=colnames(mydata),ylab="Frecuencia",breaks=19,col="green",border="red"),...)
+plot(h,freq=F,labels=labs,ylim=c(0,1.08*max(h$density)),xlab=colnames(mydata),ylab="Frecuencia",breaks=19,col="green",border="red",main=paste("Histograma de ",colnames(mydata)))
 }
