3d0751ee44c86e67876004563dd2a4fb3c7ee39b
amarent <amarent@hotmail.com>
2014-12-23 17:01:24 -0600

    1223 25

--- R/histograma.R
+++ R/histograma.R
@@ -1,11 +1,11 @@
 histograma <- function(rubro, mes, n)
 {
-  mydata<-getdata98(rubro,mes,n)
+#  mydata<-getdata98(rubro,mes,n)
 
 
 
 
-#mydata<-c(2,3,4,56,3,2,24,8,4,3,23,456,78,64,3,56,89,89)
+mydata<-c(2,3,4,56,3,2,24,8,4,3,23,456,78,64,3,56,89,89)
 h<-hist(mydata,plot=F)
 h$density<-with(h,100*density*diff(breaks)[1])
 labs<-paste(round(h$density),"%",sep="")
