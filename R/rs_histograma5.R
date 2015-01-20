57e1c8cae44b087b010e8cfa638247a883a8f419
amarent <amarent@hotmail.com>
2015-01-20 13:31:56 -0600

    2001 5

---
+++ R/rs_histograma5.R
@@ -0,0 +1,88 @@
+rs_histograma5<-function(x="2014-12-12",y="2014-12-25",k=.95){
+	
+	library(tm)
+	library(rmongodb) 
+	library(plyr)
+	library(igraph)
+	mongo <- mongo.create()
+	DBNS <- "twitter.jr_mov"
+
+	query <- mongo.bson.buffer.create()
+	mongo.bson.buffer.start.object(query, 'created_at')
+	mongo.bson.buffer.append.time(query, "$lte", strptime(y,"%Y-%m-%d"))
+	mongo.bson.buffer.finish.object(query)
+	mongo.bson.buffer.start.object(query, 'created_at')
+	mongo.bson.buffer.append.time(query, "$gte", strptime(x,"%Y-%m-%d"))
+	mongo.bson.buffer.finish.object(query)
+	query <-mongo.bson.from.buffer(query)
+
+	fields <-mongo.bson.buffer.create()
+	mongo.bson.buffer.append(fields, "text", 1L)
+	mongo.bson.buffer.append(fields, "id", 1L)
+	mongo.bson.buffer.append(fields, "_id", 0L)
+	fields <- mongo.bson.from.buffer(fields)
+
+	twts <- mongo.find(mongo, ns = DBNS, query = query,
+	 fields = fields, limit = 10000L)
+
+
+	gids <- data.frame(stringsAsFactors = FALSE)
+	while (mongo.cursor.next(twts)) {
+		tmp <- mongo.bson.to.list(mongo.cursor.value(twts))
+		tmp.df <- as.data.frame(t(unlist(tmp)), stringsAsFactors = F)
+		gids <- rbind.fill(gids, tmp.df)
+		}
+
+	mach_corpus <- Corpus(VectorSource(gids$text))
+
+	stopwords = c("movistar", "movistarmx","mimovistarmx","q","d","rt",'hola','gracias', stopwords("es"))
+	tdm <- TermDocumentMatrix(mach_corpus,
+   	control = list(removePunctuation = TRUE,
+   	stopwords = stopwords,
+   	removeNumbers = TRUE, tolower = TRUE,wordLengths=c(1,Inf)))
+	m <- as.matrix(tdm)
+	word_freqs <- sort(rowSums(m), decreasing=TRUE) 
+	dm <- data.frame(word=names(word_freqs), freq=word_freqs)
+	termDocMatrix<-m[dm[1:k,1],]
+	distMatrix <- dist(scale(termDocMatrix))
+	fit <- hclust(distMatrix, method="ward")
+	
+	
+	return(plot(fit))
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
