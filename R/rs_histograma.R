2229ff5d775e231f03e690375ad59fc83078229c
amarent <amarent@hotmail.com>
2015-01-26 11:11:41 -0600

    2601 2

--- R/rs_histograma.R
+++ R/rs_histograma.R
@@ -42,5 +42,5 @@ colnames(mydata)<-c("text")
 	head(dm,k)
 	par(las=2)
 	par(mar=c(5,8,4,2))
-	barplot(dm[1:k,2],horiz=TRUE,names.arg=dm[1:k,1],col="darkblue")
+	barplot(dm[1:15,2],horiz=TRUE,names.arg=dm[1:15,1],col="darkblue")
 }
